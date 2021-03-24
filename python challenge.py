1b47014f211b079a27b6ace4ba1ba27c42dbaea9
mlepicar <81260911+mlepicar@users.noreply.github.com>
2021-03-23 18:10:02 -0600

    Reformat

--- python challenge.py
+++ python challenge.py
@@ -5,30 +5,37 @@ ia = IMDb()
 
 pretty = PrettyTable()
 
-lista = ["Minari", "Sound of Metal", "Mank", "Promising Young Woman", "The Father", "Judas and the Black Messiah","The Trial of the Chicago 7","Nomadland"]
+lista = ["Minari","Matrix", "Sound of Metal", "Mank", "Promising Young Woman", "The Father", "Judas and the Black Messiah","The Trial of the Chicago 7","Nomadland"]
 
-pretty.field_names = ["Year","Rating","Director (s)","Plot","Cast (top 5)","Genre"]
+pretty.field_names = ["Tittle", "Year","Rating","Director (s)","Plot","Cast (top 5)","Genre"]
 
 
 for mo in lista:
-    directores = []
-    actores = []
+    directores = ""
+    actores = ""
+    c = []
+    d = []
     result = ia.search_movie(mo)
     result = result[0].movieID
     movie = ia.get_movie(result)
+    tittle = mo
     year =movie["year"]
     plot = movie["plot"]
     rating = movie["rating"]
-    director = movie["director"]
-    cast = movie["cast"]
+    director = movie["director"][0:5]
+    for dir in director:
+        d.append(dir["name"])
+    directores = ", ".join(d)
+    
+    cast = movie["cast"][0:5]
+    for cas in cast:
+        c.append(cas["name"])
+    actores= ", ".join(c)
+    
     genre = movie["genre"]
-    for d in director:
-        director = ia.get_person(d.personID)
-        directores.append(director["name"])
-    for c in range(0,5):
-        person = ia.get_person(cast[c].personID)
-        actores.append(person["name"])
-    pretty.add_row([year,rating,directores[0],plot[0],actores,genre[0]])
+
+    
+    pretty.add_row([tittle,year,rating,directores,plot[0],actores,genre[0]])
 x = pretty.get_string()
 with open('output.txt','w') as archivo_n:
     archivo_n.writelines(x)
